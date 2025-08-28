e9fe4cc93017cebb76af8f9329d8869df66d21eb
andreasLoetzsch <andreas.lotzsch@gmail.com>
2025-08-28 13:50:25 +0200

    fixed: so that coin fits

--- src/components/BoardSquare.jsx
+++ src/components/BoardSquare.jsx
@@ -1,10 +1,11 @@
 import "./boardSquare.css"
+import Coin from "./Coin/Coin"
 
 export const BoardSquare = () => {
     return(
         <div className="board-square-container">
             <div className="coin-container">
-                {/* <Coin/> */}
+                <Coin color={'red'}/>
             </div>
         </div>
     )
