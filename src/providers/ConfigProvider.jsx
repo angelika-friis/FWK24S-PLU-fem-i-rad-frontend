f9f0c71bd3ca47c2b0ef6503299797e460d1533d
Hampus von Horn <hampus.vh@gmail.com>
2025-09-11 16:17:56 +0200

    Fix ConfigProvider

--- src/providers/ConfigProvider.jsx
+++ src/providers/ConfigProvider.jsx
@@ -1,9 +1,1 @@
-import { createContext } from "react";
-
-export const ConfigContext = createContext();
-
-export const ConfigProvider = ({ children }) => (
-    <ConfigContext.Provider value={{}}>
-        {children}
-    </ConfigContext.Provider>
-);
+export const ConfigProvider = ({ children }) => children;
