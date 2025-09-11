f39ce59add373e17eb81ba8742ccdf4348734e2b
Hampus von Horn <hampus.vh@gmail.com>
2025-09-11 16:18:46 +0200

    BrowserRouter moved from App.jsx --> main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
 import AppProvider from './providers/AppProvider.jsx';
 import Router from './routes/router.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppProvider>
-          <Router />
-      </AppProvider>
-    </BrowserRouter>
+    <AppProvider>
+      <Router />
+    </AppProvider>
   )
 }
 
