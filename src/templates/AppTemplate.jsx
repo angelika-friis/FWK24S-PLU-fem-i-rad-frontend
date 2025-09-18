004683d1cd3be5920e789b9cf25285218751e56d
angelika-friis <angelikafriis@gmail.com>
2025-09-18 20:59:14 +0200

    added Header and Footer to AppTemplate

--- src/templates/AppTemplate.jsx
+++ src/templates/AppTemplate.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import styles from "./AppTemplate.module.css";
+import { Header, Footer } from "5irad-components";
 
 const AppTemplate = () => {
     return (
         <div className={styles.appTemplate}>
+            <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 }
