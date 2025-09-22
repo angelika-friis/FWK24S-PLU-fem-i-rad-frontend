c72fb63ccf8cf8bfd45ab51ab5fe7c56befd0c34
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-22 19:51:54 +0200

    added StylesProvider.jsx and StylesProvider.css

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -4,12 +4,17 @@ import { ConfigProvider } from './ConfigProvider';
 import { AuthProvider } from './AuthProvider';
 import ApiProvider from './ApiProvider';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from './StylesProvider';
 
 const AppProvider = ({ children }) => (
     <ConfigProvider>
         <AuthProvider>
             <BrowserRouter>
-                <ApiProvider>{children}</ApiProvider>
+                <ApiProvider>
+                    <StylesProvider>
+                        {children}
+                    </StylesProvider>
+                </ApiProvider>
             </BrowserRouter>
         </AuthProvider>
     </ConfigProvider>
---
+++ src/providers/StylesProvider.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/StylesProvider.css";
+
+export const StylesProvider = ({ children }) => {
+    return <>{children}</>;
+};
