12d58a2cf24cd969fe9f9514b962984fb4215fdf
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-22 19:45:42 +0200

    fixed possible react import dupe

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  resolve: { dedupe: ["react", "react-dom"] },
   test: {
     projects: [{
       extends: true,
