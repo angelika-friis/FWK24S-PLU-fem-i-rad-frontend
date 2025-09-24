6a9d1986bc67b8373458588b7b1141a30784ea04
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-24 21:49:10 +0200

    passed user state down to menu

--- src/templates/AppTemplate.jsx
+++ src/templates/AppTemplate.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import styles from "./AppTemplate.module.css";
 import { Menu, Footer } from "5irad-components";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const AppTemplate = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <div className={styles.appTemplate}>
-            <Menu />
+            <Menu user={user} />
             
             <div className={styles.appWrapper}>
                 <Outlet />
