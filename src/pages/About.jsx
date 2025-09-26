fc3a6ca69b4190d5744898dff9e41fd018a8e432
Hampus von Horn <hampus.vh@gmail.com>
2025-09-26 12:45:28 +0200

    About.jsx prototype

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <section className="about-content">
+            <h1>Our vision</h1>
+            <p>This project started with a simple idea:</p>
+            <blockquote>"As a player, I want to play a game of Gomoku so that I can win, lose or draw."</blockquote>
+            <p>That's it. Just a classic five-in-a-row game, brought online so we can challenge each other and have some fun.</p>
+
+        </section>
+    )
+}
+
+export default About;
