dbd6a2a48019d053ad5a42fe02b4b10af70b3e95
Hampus von Horn <hampus.vh@gmail.com>
2025-09-26 21:57:55 +0200

    Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,12 @@
+import { ContactForm } from "5irad-components";
+
 const Contact = () => {
     return (
-        <div>
-        </div>
-    )
-}
+        <section>
+            <h1>Contact us (in progress)</h1>
+            <ContactForm />
+        </section>
+    );
+};
 
 export default Contact;
