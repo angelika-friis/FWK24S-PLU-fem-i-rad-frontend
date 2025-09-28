4459905a86530e364ddf4a08f3d4f7096194fd4a
Hampus von Horn <hampus.vh@gmail.com>
2025-09-28 12:31:39 +0200

    Contact title moved --> components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import { ContactForm } from "5irad-components";
 const Contact = () => {
     return (
         <section>
-            <h1>Contact us (in progress)</h1>
             <ContactForm />
         </section>
     );
